6c0307eb9c63d23e48f05ef0d45e87965c9c1c42
MattiasKDev <mattias@kerner.ca>
2023-09-24 12:16:47 -0400

    Implemented smart auto heal

--- scripts/smartAutoHeal.js
+++ scripts/smartAutoHeal.js
@@ -1,3 +1,35 @@
+/**
+ * Author: infinity
+ * Name: Smart Auto Heal
+ * Description: Automatically uses hp pots for you.
+ */
+
 if (JSON.parse(localStorage.getItem("scriptSettings")).smartAutoHeal) main();
 
-function main() {}
+function main() {
+	usePotWithCooldown();
+}
+
+async function usePotWithCooldown() {
+	let cooldown = await usePot();
+	setTimeout(usePotWithCooldown, (cooldown + 1) * 1000);
+}
+
+async function usePot() {
+	let secondsUntilNextPot;
+	await fetch(
+		"https://api.dragonsofthevoid.com/api/usable/consume/u.healing-potion",
+		{ headers: { authorization: this.localStorage.token } }
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.success) {
+				secondsUntilNextPot = 360;
+			} else {
+				secondsUntilNextPot = "t";
+				secondsUntilNextPot = parseInt(data.errorMsg.match(/\d+/)[0]);
+			}
+		});
+
+	return secondsUntilNextPot;
+}
